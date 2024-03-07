c29df357bb0ff3a277e8b29cba14aa4d8fc2a6a2
Prasad Somvanshi <99985037+Prasad5701@users.noreply.github.com>
2024-03-07 09:47:22 +0530

    Add files via upload

---
+++ 17_Constructor/SuperThisCall2.java
@@ -0,0 +1,32 @@
+class Parent
+{
+	Parent()
+	{
+		super();
+		System.out.println("Hello");
+
+	}
+	Parent(int a)
+	{
+		Parent45();
+		System.out.println("hii");
+	}
+}
+
+class Child extends Parent
+{
+	child()
+	{
+		child(10);
+		System.out.println("nothing");
+	}
+	child(int a)
+	{
+		super(int 30);
+		System.out.println("Something");
+
+	}
+	public static void main(String[] args) {
+		Child a = new Child();
+	}
+}
